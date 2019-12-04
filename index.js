9098a4da9972d794a547cedcd70948c09235b51d
ragtam <mag190033@gmail.com>
2019-12-04 15:57:11 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Scope
 */
 
 // RHS reference cannot be found inside foo function, but it is found in surrounding scope ( global )
+/*
 function foo(a) {
   console.log(a + b);
 }
@@ -12,7 +13,10 @@ foo(3); // 5
 function foo2(a) {
   console.log(a + b1);
 }
-//foo2(3); // Reference Error. If RHS lookup fails since b1 is not defined, it will throw an error.
+foo2(3); // Reference Error. If RHS lookup fails since b1 is not defined, it will throw an error.
+*/
+
+
 /*
 in LHS, by contrast, won`t find a variable until global scope, it will be helpful and create one.
 if strict mode switched on, Reference Error will be thrown too.
